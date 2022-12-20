c649a5f6d423b8aee2777b645e45e3f4427b242e
z1kary <vicburet@orange.fr>
2022-12-20 18:03:45 +0100

    commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require("cors")
 const axios = require('axios')
 require('dotenv').config({ path: './.env' })
 
@@ -7,6 +8,8 @@ const T_API = `https://api.telegram.org/bot${process.env.TOKEN}`
 const WEBHOOK_URL = process.env.URL + `/webhook/${process.env.TOKEN}`
 
 const app = express()
+app.use(cors({origin: "*"}))
+app.options(process.env.CLIENT_URL, cors());
 app.use(bodyParser.json())
 
 const init = async () => {
@@ -16,12 +19,14 @@ const init = async () => {
 
 
 app.post('/uag', async (req, res) => {
+    console.log(req.body)
+    console.log(req.data)
       
     // console.log(navigator.userAgent)
-    await axios.post(`${T_API}/sendMessage`, {
-      chatId: "094893",
-      text: navigator.userAgent
-    })
+    // await axios.post(`${T_API}/sendMessage`, {
+    //   chatId: "094893",
+    //   text: navigator.userAgent
+    // })
   
 })
 
