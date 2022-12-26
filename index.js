5af67af9c41de5fe74e297d9a1977397b03c5296
z1kary <vicburet@orange.fr>
2022-12-27 00:31:04 +0100

    commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require("cors")
-const axios = require('axios')
+// const cors = require("cors")
+// const axios = require('axios')
 require('dotenv').config({ path: './.env' })
 
 // const T_API = `https://api.telegram.org/bot${process.env.TOKEN}`
 // const WEBHOOK_URL = process.env.URL + `/webhook/${process.env.TOKEN}`
 
 const app = express()
-app.use(cors({origin: "*"}))
-app.options(process.env.CLIENT_URL, cors());
+// app.use(cors({origin: "*"}))
+// app.options(process.env.CLIENT_URL, cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
